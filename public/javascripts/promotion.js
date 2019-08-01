0db286c7fceb4c38405964ea57964d34b1b0aa0c
jhoijune <jhoijune@gmail.com>
2019-08-01 20:30:15 +0900

    I did some front end work related to autoscrolling.

---
+++ public/javascripts/promotion.js
@@ -0,0 +1,59 @@
+let scrollAble;
+
+function autoScroll(delay = 1){
+    const promotionList = document.querySelector(".promotion ul");
+    const len = promotionList.querySelectorAll("li").length;
+    const item = promotionList.firstElementChild;
+    let degree = 0;
+    setInterval(()=> {
+        if(scrollAble){
+            degree = (degree + 1) % len;
+            const interval = item.clientWidth;
+            promotionList.style.transform = `translateX(-${interval*degree}px)`;
+        }
+    },delay*1000);
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function(){
+        if(xhr.status === 200){
+            const datas = JSON.parse(xhr.responseText).items;
+            const promotionList = document.createElement("ul");
+            for(i=0,len=datas.length;i<len;i++){
+                const item = document.createElement("li");
+                const anchor = document.createElement("a");
+                anchor.href = datas[i].productID; // 임시
+                const image = document.createElement("img");
+                image.src = datas[i].productImageUrl;
+                anchor.appendChild(image);
+                item.appendChild(anchor);
+                promotionList.appendChild(item);
+            }
+            const promotionContainer = document.querySelector(".promotion .container");
+            promotionContainer.appendChild(promotionList);
+            scrollAble = true;
+            autoScroll(2);
+            window.addEventListener("resize",(event)=>{
+                // 즉시 멈추고 resize한 width에 맞게 translateX를 바꿔야 함
+                // resize 끝나면 다시 autoScroll
+                scrollAble = false;
+            });
+            promotionList.addEventListener("mousemove",()=>{
+                scrollAble = false;
+            });
+            promotionList.addEventListener("mouseleave",(event)=>{
+                scrollAble = true;
+                // 즉시 스크롤 되게 바꿔야함
+            });
+        }    
+        else{
+            console.error(xhr.responseText);
+        }
+    };
+    xhr.onerror = function(){
+        console.error(xhr.responseText);
+    }
+    xhr.open("GET","/api/promotions");
+    xhr.send();
+});
